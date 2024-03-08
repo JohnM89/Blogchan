899c4cd77d903db07625b8c672b7f9f4e3d59d78
JohnM89 <mjammer2@gmail.com>
2024-03-08 07:00:38 -0400

    ok

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 
-
 const db = {};
 
 db.Sequelize = Sequelize;
@@ -11,13 +10,25 @@ db.User = require('./User');
 db.BlogPost = require('./BlogPost');
 db.Comment = require('./Comment');
 
-
 Object.keys(db).forEach((modelName) => {
-
-  
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
+});
+
+// Add your migration script here
+const queryInterface = sequelize.getQueryInterface();
+
+queryInterface.addConstraint('blogPost', {
+  fields: ['authorId'],
+  type: 'foreign key',
+  references: {
+    table: 'User',
+    field: 'id'
+  },
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE',
+  name: 'custom_authorId_foreign_key_constraint' // Replace with your desired custom name
 });
 
 module.exports = db;
