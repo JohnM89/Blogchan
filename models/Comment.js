dc6d1247d88bfa24de5250e011d4c07d0ff8e213
JohnM89 <mjammer2@gmail.com>
2024-03-08 04:37:37 -0400

    ok

--- models/Comment.js
+++ models/Comment.js
@@ -1,67 +1,89 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Comment extends Model {}
+class Comment extends Model { }
 
-Comment.init(
-  {
+Comment.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     commentText: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     dateCreated: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: DataTypes.NOW,
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: DataTypes.NOW,
     },
     upVotes: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0,
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
     },
+
     downVotes: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0,
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
     },
+    //       userId: {
+    //     type: DataTypes.INTEGER, 
+    //     allowNull: false
+    //   },
     authorId: {
-      type: DataTypes.INTEGER,
-      allowNull: false, // Changed to allowNull: false
-      references: {
-        model: 'User', // Changed to 'User'
-        key: 'id',
-      },
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: 'User',
+            key: 'id',
+        },
+
+        // authorUsername: {
+        //     type: DataTypes.STRING,
+        //     allowNull: true,
+        //     references: {
+        //         model: 'user',
+        //         key: 'username',
+
+        //     },    
+
+        // },
+        blogPostId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: 'blogPost',
+                key: 'id',
+            },
+        },
     },
-    blogPostId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'BlogPost',
-        key: 'id',
-      },
-    },
-  },
-  {
+}, {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'Comment',
-  }
-);
+    modelName: 'comment',
+});
 
 Comment.associate = (models) => {
-  Comment.belongsTo(models.BlogPost, {
-    foreignKey: 'blogPostId',
-  });
-  Comment.belongsTo(models.User, {
-    foreignKey: 'authorId',
-  });
+    Comment.belongsTo(models.BlogPost, {
+        foreignKey: 'blogPostId',
+    });
+    Comment.belongsTo(models.User, {
+        foreignKey: 'authorId',
+    });
+
+
+
+
+    // Comment.belongsTo(models.User, {
+    //     foreignKey: 'authorUsername',
+
+    // });
+
 };
 
 module.exports = Comment;
