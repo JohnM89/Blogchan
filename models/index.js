6ae7b271091fe479e529c2df9ac90142d83b2378
JohnM89 <mjammer2@gmail.com>
2024-03-08 07:08:26 -0400

    ok

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 
+
 const db = {};
 
 db.Sequelize = Sequelize;
@@ -10,30 +11,13 @@ db.User = require('./User');
 db.BlogPost = require('./BlogPost');
 db.Comment = require('./Comment');
 
+
 Object.keys(db).forEach((modelName) => {
+
+  
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-});
-
-// Modify your migration script to run after the blogPost table is created
-sequelize.sync({ force: true }).then(() => {
-  console.log('Database synchronized');
-}).then(() => {
-  const queryInterface = sequelize.getQueryInterface();
-  queryInterface.addConstraint('blogPost', {
-    fields: ['authorId'],
-    type: 'foreign key',
-    references: {
-      table: 'User',
-      field: 'id'
-    },
-    onUpdate: 'CASCADE',
-    onDelete: 'CASCADE',
-    name: 'authorId' // Replace with your desired custom name
-  });
-}).catch((error) => {
-  console.error('Error synchronizing database:', error);
 });
 
 module.exports = db;
