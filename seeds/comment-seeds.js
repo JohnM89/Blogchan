194cc80f95d83c8ee7de5398e74a8fddf195eb84
JohnM89 <mjammer2@gmail.com>
2024-03-08 06:12:20 -0400

    styling

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,21 +1,21 @@
 const commentData = [
   {
-    // commentText: "LGTM!",
-    // authorId: 1,
-    // // userId: 1,
-    // upVotes: 50,
-    // downVotes: 1,
-    // dateCreated: "2024-01-01",
-    // blogPostId: 1
+    commentText: "LGTM!",
+    authorId: 1,
+    // userId: 1,
+    upVotes: 50,
+    downVotes: 1,
+    dateCreated: "2024-01-01",
+    blogPostId: 1
   },
   {
-    // commentText: "Make Crypto fast with this one weird trick!",
-    // authorId: 2,
-    // // userId: 2,
-    // upVotes: 100,
-    // downVotes: 0,
-    // dateCreated: "2024-01-01",
-    // blogPostId: 1
+    commentText: "Make Crypto fast with this one weird trick!",
+    authorId: 2,
+    // userId: 2,
+    upVotes: 100,
+    downVotes: 0,
+    dateCreated: "2024-01-01",
+    blogPostId: 1
   }
 ];
 
