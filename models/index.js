8b1a515314d02b33d26a07463e86fa46765996df
JohnM89 <mjammer2@gmail.com>
2024-03-08 04:44:48 -0400

    ok

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/connection');
 
-
 const db = {};
 
 db.Sequelize = Sequelize;
@@ -11,10 +10,16 @@ db.User = require('./User');
 db.BlogPost = require('./BlogPost');
 db.Comment = require('./Comment');
 
+// constraint issue solution
+db.Comment.belongsTo(db.User, {
+  foreignKey: 'authorId',
+  constraints: true, 
+  onDelete: 'CASCADE', 
+  onUpdate: 'CASCADE', 
+  name: 'fk_author_id' 
+});
 
 Object.keys(db).forEach((modelName) => {
-
-  
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
